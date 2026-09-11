c28cc9d1aec49f744881fdedb46f742e28034358
zu2 <3023910+zu2@users.noreply.github.com>
2026-09-11 16:19:08 +0000

    mc6800: main.c add the --no-std-crt0 option

--- src/mc6800/main.c
+++ src/mc6800/main.c
@@ -262,12 +262,14 @@ _mc6800_parseOptions (int *pargc, char **argv, int *i)
 
 #define OPTION_SMALL_MODEL          "--model-small"
 #define OPTION_LARGE_MODEL          "--model-large"
+#define OPTION_NO_STD_CRT0          "--no-std-crt0"
 
 static OPTION _mc6800_options[] =
   {
     {0, OPTION_SMALL_MODEL, NULL, "8-bit address space for data"},
     {0, OPTION_LARGE_MODEL, NULL, "16-bit address space for data (default)"},
     {0, "--out-fmt-elf", NULL, "Output executable in ELF format" },
+    {0, OPTION_NO_STD_CRT0, &options.no_std_crt0, "Do not link default crt0.rel"},
     {0, NULL }
   };
 
