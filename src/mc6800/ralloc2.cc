4897d76cb331993152552a2dcc917b96025d1c0b
zu2 <3023910+zu2@users.noreply.github.com>
2026-09-13 14:27:32 +0000

    mc6800: gen.c: Fix incorrect IX register allocation in assignment_hopeless

--- src/mc6800/ralloc2.cc
+++ src/mc6800/ralloc2.cc
@@ -493,7 +493,7 @@ static bool assignment_hopeless(const assignment &a, unsigned short int i, const
 {
   const i_assignment_t &ia = a.i_assignment;
 
-  if((ia.registers[REG_XL][1] >= 0 || ia.registers[REG_XH][1] >= 0) &&
+  if((ia.registers[REG_XL][1] >= 0 && ia.registers[REG_XH][1] >= 0) &&
       !Xinst_ok(a, i, G, I))
     return(true);
 
