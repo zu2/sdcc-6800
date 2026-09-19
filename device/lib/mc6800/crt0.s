3f743f14a06741306ce18acf70216086559fc908
zu2 <3023910+zu2@users.noreply.github.com>
2026-09-19 23:51:37 +0000

    mc6800: move the initialization code into crt0.s
    - Remove _xinit.s

--- device/lib/mc6800/crt0.s
+++ device/lib/mc6800/crt0.s
@@ -1,7 +1,16 @@
 	.module crt0
 	.globl _main
 	.globl ___sdcc_external_startup
-	.globl __sdcc_init_xdata
+	.globl s_ZP
+	.globl l_ZP
+	.globl s_XSEG
+	.globl l_XSEG
+	.globl s_XINIT
+	.globl l_XINIT
+	.globl s_XISEG
+	.globl ___SDCC_mc6800_tmp0
+	.globl ___SDCC_mc6800_tmp1
+	.globl ___SDCC_mc6800_tmp2
 	.globl __sdcc_stack_top
 
 SIMIF	=	0xfefe
@@ -38,8 +47,75 @@ __sdcc_gs_init_startup:
 	jsr	___sdcc_external_startup
 	beq	__sdcc_init_data
 	jmp	__sdcc_program_startup
+;
 __sdcc_init_data:
-	jsr	__sdcc_init_xdata
+	ldab	#<l_ZP			; assume l_ZP < 256
+	beq	00002$
+	ldx	#s_ZP
+	clra
+00001$:
+	staa	0,x
+	inx
+	decb
+	bne	00001$
+;
+00002$:
+	ldx	#l_XSEG
+	beq	00004$
+	stx	*___SDCC_mc6800_tmp0
+	ldx	#s_XSEG
+	ldab	#<l_XSEG
+	beq	00003$
+	inc	___SDCC_mc6800_tmp0
+;
+00003$:
+	staa	0,x
+	inx
+	decb
+	bne	00003$
+	dec	___SDCC_mc6800_tmp0
+	bne	00003$
+;
+00004$:
+	ldab	#<s_XISEG
+	addb	#<l_XINIT
+	ldaa	#>s_XISEG
+	adca	#>l_XINIT
+	stab	*___SDCC_mc6800_tmp2+1
+	staa	*___SDCC_mc6800_tmp2
+;
+	ldx	#s_XINIT
+	ldab	#<l_XINIT
+	lsrb
+	bcc	00007$
+	ldaa	0,x
+	inx
+;
+00007$:
+	stx	*___SDCC_mc6800_tmp0
+	ldx	#s_XISEG
+	bcc	00009$
+	staa	0,x
+	inx
+	bra	00009$
+;
+00005$:
+	stx	*___SDCC_mc6800_tmp1
+	ldx	*___SDCC_mc6800_tmp0
+	ldaa	0,x
+	ldab	1,x
+	inx
+	inx
+	stx	*___SDCC_mc6800_tmp0
+	ldx	*___SDCC_mc6800_tmp1
+	staa	0,x
+	stab	1,x
+	inx
+	inx
+;
+00009$:
+	cpx	*___SDCC_mc6800_tmp2
+	bne	00005$
 
 	.area GSFINAL
 	jmp	__sdcc_program_startup
