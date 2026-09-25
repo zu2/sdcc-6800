a0a7d0b5a1b4ad32d92d7220ce284d0ead9163ec
zu2 <3023910+zu2@users.noreply.github.com>
2026-09-25 04:52:03 +0000

    mc6800: use clr for the upper byte of X in genCopy

--- src/mc6800/gen.c
+++ src/mc6800/gen.c
@@ -2815,21 +2815,23 @@ genCopy (operand *result, operand *source)
       if (IS_AOP_X (AOP (result)) && size == 2 && srcsize == 1)
         {
           const char *tmp = allocTemp ();
-          bool useb = (AOP_TYPE (source) == AOP_REG && AOP (source)->aopu.aop_reg[0] == mc6800_reg_b);
-          bool pula = pushRegIfUsed (mc6800_reg_a);
 
-          if (useb)
+          if (AOP_TYPE (source) == AOP_REG && AOP (source)->aopu.aop_reg[0] == mc6800_reg_b)
             mc6800_emitOp ("stab", MODE_DIR, "*%s+1", tmp);
+          else if (AOP_TYPE (source) == AOP_REG && AOP (source)->aopu.aop_reg[0] == mc6800_reg_a)
+            mc6800_emitOp ("staa", MODE_DIR, "*%s+1", tmp);
           else
             {
-              loadRegFromAop (mc6800_reg_a, AOP (source), 0);
-              mc6800_emitOp ("staa", MODE_DIR, "*%s+1", tmp);
+              bool usea = mc6800_reg_a->isFree || !mc6800_reg_b->isFree;
+              bool needpullacc = pushRegIfUsed (usea ? mc6800_reg_a : mc6800_reg_b);
+
+              loadRegFromAop (usea ? mc6800_reg_a : mc6800_reg_b, AOP (source), 0);
+              mc6800_emitOp (usea ? "staa" : "stab", MODE_DIR, "*%s+1", tmp);
+              pullOrFreeReg (usea ? mc6800_reg_a : mc6800_reg_b, needpullacc);
             }
-          loadRegFromConst (mc6800_reg_a, 0);
-          mc6800_emitOp ("staa", MODE_DIR, "*%s", tmp);
+          mc6800_emitOp ("clr", MODE_EXT, "%s", tmp);
           mc6800_emitOp ("ldx", MODE_DIR, "*%s", tmp);
           freeTemp ();
-          pullOrFreeReg (mc6800_reg_a, pula);
           mc6800_dirtyReg (mc6800_reg_x, false);
           return;
         }
